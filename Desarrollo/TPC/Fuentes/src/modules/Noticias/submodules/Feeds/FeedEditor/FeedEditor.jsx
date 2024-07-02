af0dd4225c3986aa8b0b008706874cc68d43b0c8
feelware <stevanpersonal@gmail.com>
2024-07-02 02:43:27 -0500

    feat: add validation to URL input of feed editor

--- Desarrollo/TPC/Fuentes/src/modules/Noticias/submodules/Feeds/FeedEditor/FeedEditor.jsx
+++ Desarrollo/TPC/Fuentes/src/modules/Noticias/submodules/Feeds/FeedEditor/FeedEditor.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import {
   Stack,
   Group,
@@ -8,6 +6,11 @@ import {
   Button
 } from '@mantine/core'
 
+import {
+  useForm,
+  matches
+} from '@mantine/form'
+
 const FeedEditor = ({
   opened,
   onClose,
@@ -15,7 +18,11 @@ const FeedEditor = ({
   url = '',
   sendLabel
 }) => {
-  const [newUrl, setNewUrl] = useState(url)
+  // const [newUrl, setNewUrl] = useState(url)
+  const form = useForm({
+    initialValues: { newUrl: url },
+    validate: { newUrl: matches(/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/, 'URL inválida') }
+  })
 
   return (
     <Modal
@@ -24,33 +31,34 @@ const FeedEditor = ({
       centered
       withCloseButton={false}
     >
-      <Stack>
-        <TextInput 
-          label='URL del feed'
-          placeholder='https://www.ejemplo.com'
-          value={newUrl}
-          onChange={(e) => setNewUrl(e.target.value)}
-        />
-        <Group justify='space-between'>
-          <Button
-            variant='default'
-            aria-label="Cancelar"
-            onClick={onClose}
-          >
-            Cancelar
-          </Button>
-          <Button
-            variant='filled'
-            aria-label={sendLabel}
-            onClick={() => {
-              onSend(newUrl)
-              setNewUrl('')
-            }}
-          >
-            {sendLabel}
-          </Button>
-        </Group>
-      </Stack>
+      <form onSubmit={form.onSubmit(async ({ newUrl }) => {
+        await onSend(newUrl)
+      })}>
+        <Stack>
+          <TextInput 
+            label='URL del feed'
+            placeholder='https://www.ejemplo.com'
+            key={form.key('newUrl')}
+            {...form.getInputProps('newUrl')}
+          />
+          <Group justify='space-between'>
+            <Button
+              variant='default'
+              aria-label="Cancelar"
+              onClick={onClose}
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant='filled'
+              aria-label={sendLabel}
+              type='submit'
+            >
+              {sendLabel}
+            </Button>
+          </Group>
+        </Stack>
+      </form>
     </Modal>
   )
 }
