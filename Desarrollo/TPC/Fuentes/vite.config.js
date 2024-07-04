c9274ea877bed44d0a2b8dccbcf402f68d78861b
Jose Zegarra <jzegarram2000@gmail.com>
2024-07-04 02:36:51 -0500

    feat: adding background scripting config

--- Desarrollo/TPC/Fuentes/vite.config.js
+++ Desarrollo/TPC/Fuentes/vite.config.js
@@ -9,6 +9,22 @@ export default defineConfig({
     react(),
     crx({ manifest }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'src/scripts/background.js',
+        content: 'src/scripts/content.js'
+      },
+      output: {
+        entryFileNames: ({ name }) => {
+          if (name === 'background') return 'background.js';
+          if (name === 'content') return 'content.js';
+          return '[name].js';
+        }
+      }
+    },
+  },
   server: {
     port: 3000,
   },
