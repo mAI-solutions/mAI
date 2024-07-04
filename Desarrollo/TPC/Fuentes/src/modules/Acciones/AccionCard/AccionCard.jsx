2f6318dbee82349c0f8046b353c142d70854b4c8
PolvosMagicos <73202603+PolvosMagicos@users.noreply.github.com>
2024-07-03 20:06:28 -0500

    feat: fetch cycling tasks

--- Desarrollo/TPC/Fuentes/src/modules/Acciones/AccionCard/AccionCard.jsx
+++ Desarrollo/TPC/Fuentes/src/modules/Acciones/AccionCard/AccionCard.jsx
@@ -1,64 +1,44 @@
-import {
-  ActionIcon,
-  Group,
-  Card,
-  Text,
-  Menu,
-} from '@mantine/core'
+import { ActionIcon, Group, Card, Text, Menu } from "@mantine/core";
 
-import {
-  useDisclosure,
-} from '@mantine/hooks'
+import { useDisclosure } from "@mantine/hooks";
 
-import {
-  IconEdit,
-  IconTrash,
-  IconDots
-} from '@tabler/icons-react'
+import { IconEdit, IconTrash, IconDots } from "@tabler/icons-react";
 
-import AccionEditor from '../AccionEditor'
+import AccionEditor from "../AccionEditor";
 
 const AccionCard = ({ accion, onEdit, onDelete }) => {
-  const [
-    modalOpened, 
-    { open: modalOpen, close: modalClose }
-  ] = useDisclosure()
-  
+  const [modalOpened, { open: modalOpen, close: modalClose }] = useDisclosure();
+
   return (
     <>
       <Card>
-        <Group justify='space-between'>
+        <Group justify="space-between">
           <div>
-            <Text>
-              {accion.title}
-            </Text>
-            <Text 
-              size="xs" 
-              c="dimmed" 
-            >
-              Cada {accion.interval.frequency} {accion.interval.unit} 
+            <Text>{accion.title}</Text>
+            <Text size="xs" c="dimmed">
+              Cada {accion.interval.minutes}
             </Text>
           </div>
-          <Menu position='bottom-end'>
+          <Menu position="bottom-end">
             <Menu.Target>
               <ActionIcon
                 aria-label="Opciones"
                 title="Opciones"
-                variant='transparent'
-                color='default'
+                variant="transparent"
+                color="default"
               >
-                <IconDots size={15}/>
+                <IconDots size={15} />
               </ActionIcon>
             </Menu.Target>
             <Menu.Dropdown>
               <Menu.Item
-                leftSection={<IconEdit size={15}/>}
+                leftSection={<IconEdit size={15} />}
                 onClick={modalOpen}
               >
                 Editar
               </Menu.Item>
               <Menu.Item
-                leftSection={<IconTrash size={15}/>}
+                leftSection={<IconTrash size={15} />}
                 onClick={onDelete}
               >
                 Eliminar
@@ -72,13 +52,13 @@ const AccionCard = ({ accion, onEdit, onDelete }) => {
         onClose={modalClose}
         accion={accion}
         onSend={(newAccion) => {
-          onEdit(newAccion)
-          modalClose()
+          onEdit(newAccion);
+          modalClose();
         }}
-        sendLabel='Actualizar'
+        sendLabel="Actualizar"
       />
     </>
-  )
-}
+  );
+};
 
-export default AccionCard
+export default AccionCard;
