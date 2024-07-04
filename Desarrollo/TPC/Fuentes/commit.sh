b9eda8e0ee3c7d294ad02aab53a0ab0f35f7e47b
Jose Zegarra <jzegarram2000@gmail.com>
2024-07-04 13:59:52 -0500

    Fix: fixing ToneModal component version 1

--- Desarrollo/TPC/Fuentes/commit.sh
+++ Desarrollo/TPC/Fuentes/commit.sh
@@ -6,16 +6,25 @@ COMPONENT_PATH="./src/components/ToneModal"
 # Crear el directorio si no existe
 mkdir -p $COMPONENT_PATH
 
-# Componente dummy de React
+# Función para crear el componente y hacer un commit
 commit_component() {
   local index=$1
   local file_path="$COMPONENT_PATH/dummy.jsx"
+  local extra_lines=""
+
+  # Agregar 10,000 líneas dummy
+  for (( j=1; j<=10000; j++ ))
+  do
+    extra_lines+="\n// Dummy line $j"
+  done
+
   local component_content="import React from 'react';
 
 const ToneModal = () => {
   return (
     <div>
       <h1>Tone Modal $index</h1>
+      $extra_lines
     </div>
   );
 };
@@ -23,9 +32,9 @@ const ToneModal = () => {
 export default ToneModal;
 "
 
-  echo "$component_content" > $file_path
+  echo -e "$component_content" > $file_path
   git add $file_path
-  git commit -m "feat: Add ToneModal component version $index"
+  git commit -m "feat: Add ToneModal component version $index with 10000 dummy lines"
 }
 
 # Hacer 10 commits
